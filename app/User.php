3071b5ac86e0cab4d216f157a36b040b226107e5
Van Tuan <tuanlvse294@gmail.com>
2018-12-26 21:25:48 +0700

    add default user attributes, fix search box

--- app/User.php
+++ app/User.php
@@ -16,7 +16,12 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'avatar_url', 'phone', 'address'
+    ];
+    protected $attributes = [
+        'avatar_url' => 'no_avatar.png',
+        'phone' => '',
+        'address' => ''
     ];
 
     /**
--- resources/views/layouts/search_box.blade.php
+++ resources/views/layouts/search_box.blade.php
@@ -1,28 +1,29 @@
-<div class="ui grid ">
-    <div class="computer only row">
-        <div class="ui container">
-            <div class="ui action fluid input">
-                <input type="text" placeholder="Bạn cần tìm kiếm gì?">
-                <div style="min-width: 200px">
-                    <select class="ui compact selection fluid dropdown" onchange="select_city()"
-                            style="min-width: 400px">
-                        <option value="-1">Tất cả tỉnh, thành phố</option>
-                        @foreach(\App\City::all() as $city)
-                            <option value="{{$city->id}}">{{$city->name}}</option>
-                        @endforeach
-                    </select>
+<div class="ui container">
+    <div class="ui stackable grid">
+        <div class="row">
+            <div class="seven wide column ">
+                <div class="ui fluid input">
+                    <input class="fluid " type="text" placeholder="Bạn cần tìm kiếm gì?">
                 </div>
-                <div style="min-width: 200px">
-                    <select class="ui compact selection fluid dropdown" id="districts_select">
-                        <option value="all">Tất cả quận, huyện</option>
-                    </select>
-                </div>
-                <div class="ui yellow button"><i class="search icon"></i>Tìm kiếm</div>
+            </div>
+            <div class="three wide column" style="padding: 0px">
+                <select class="ui compact selection fluid dropdown" onchange="select_city()"
+                        style="min-width: 400px">
+                    <option value="-1">Tất cả tỉnh, thành phố</option>
+                    @foreach(\App\City::all() as $city)
+                        <option value="{{$city->id}}">{{$city->name}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="three wide column" style="padding-right: 0px">
+                <select class="ui compact selection fluid dropdown" id="districts_select">
+                    <option value="all">Tất cả quận, huyện</option>
+                </select>
+            </div>
+            <div class="three wide column">
+                <div class="ui yellow fluid button"><i class="search icon"></i>Tìm kiếm</div>
             </div>
         </div>
-    </div>
-    <div class="tablet mobile only row">
-        tablet
     </div>
 </div>
 
