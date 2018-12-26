52a289c3504b66d65d8c314f0fe57b60ee0c214c
Van Tuan <tuanlvse294@gmail.com>
2018-12-26 20:29:45 +0700

    fix title

--- resources/views/layouts/blank.blade.php
+++ resources/views/layouts/blank.blade.php
@@ -10,7 +10,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{isset($title)?$title.' - ':''}}PetStore - Thế giới thú nuôi</title>
+    <title>{{isset($title)?$title.' - ':''}}CHome - Rao vặt bất động sản</title>
 
     @include('layouts.js_include')
 
