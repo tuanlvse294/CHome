0f90375425782ab412ffb13d0009963bfe9b373e
Van Tuan <tuanlvse294@gmail.com>
2019-03-08 12:05:37 +0700

    not found result

--- resources/views/offer/list2.blade.php
+++ resources/views/offer/list2.blade.php
@@ -1,9 +1,13 @@
 <div class="ui items segment">
-    @foreach($offers as $offer)
-        @include('offer.row_item',['item'=>$offer])
-        <hr class="ui divider">
-    @endforeach
-    <div style="text-align: center">
-        {{ $offers->appends(Request::except('page'))->links('vendor.pagination.default') }}
-    </div>
+    @if($offers->count() ==0)
+        <p style="text-align: center"><b>Không tìm thấy kết quả phù hợp!!</b></p>
+    @else
+        @foreach($offers as $offer)
+            @include('offer.row_item',['item'=>$offer])
+            <hr class="ui divider">
+        @endforeach
+        <div style="text-align: center">
+            {{ $offers->appends(Request::except('page'))->links('vendor.pagination.default') }}
+        </div>
+    @endif
 </div>
