4a31835e8c4f23680edede0359921e3b88726f02
Van Tuan <tuanlvse294@gmail.com>
2019-05-02 00:42:03 +0700

    footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,22 +2,21 @@
 <div style="background: black;padding-top:10px;padding-bottom: 50px;" id="xfooter">
     <div class="ui container">
         <div class="ui inverted secondary menu stackable three column grid">
-            <div class="column"></div>
+            <div class="column">
+                <p class="ui dividing header item"><strong>Contact</strong></p>
+                <p class="item">Email: quanghnse04435@fpt.edu.vn</p>
+                <p class="item">Hotline: 0963.501.365 - 0975.110.096</p>
+            </div>
             <div class="column">
                 <p class="ui dividing header item"><strong>Copyright</strong></p>
-                <p class="item"> C-Home © 2018. All Rights Reserved.</p>
+                <p class="item"> © Copyright 2019 Chome Inc. </p>
+                <p class="item">All Rights Reserved</p>
             </div>
-            {{--<div class="column">--}}
-            {{--<p class="ui dividing header item"><strong>Contact</strong></p>--}}
-            {{--<p class="item" >Email: {{Setting::get('email','not_set@gmail.com')}}</p>--}}
-            {{--<p class="item" >Phone: {{Setting::get('phone','000000')}}</p>--}}
-            {{--</div>--}}
-            {{--<div class="column">--}}
-            {{--<p class="ui  dividing header item"><strong>Liên kết</strong></p>--}}
-            {{--@foreach(Setting::get('more_links',[]) as $label=>$link)--}}
-            {{--<a class="item" href="{{$link}}">{{$label}}</a>--}}
-            {{--@endforeach--}}
-            {{--</div>--}}
+            <div class="column">
+                <p class="item" >Xin chân thành cám ơn Quý khách đã quan tâm đến dự án CHOME.</p>
+                <p class="item" >CHOME.XYZ – MUA BÁN NHÀ ĐẤT</p>
+                <p class="item" >CÔNG TY TNHH ĐTQD </p>
+            </div>
         </div>
         <h1 style="color: white;text-align: center">
             <span style="border: solid 2px white;padding: 10px">C-Home.xyz</span></h1>
